d6020a8f81604cda690c608b5e556679b4e3f484
yan-happys <64684316+yan-happys@users.noreply.github.com>
2023-03-30 13:41:37 +0800

    Add files via upload

---
+++ src/test/java/com/example/pipelinedemo/PipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipelinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
